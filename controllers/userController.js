efe6490db6b8d2a4e09d31521e3831a253f63c6d
sujul2905 <51090620+kurokami2905@users.noreply.github.com>
2020-10-20 23:33:40 +0700

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,49 @@
-const { promisify } = require('util');
-const jwt = require('jsonwebtoken');
-const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError');
-const factory = require('./handlerFactory');
+const { promisify } = require("util");
+const jwt = require("jsonwebtoken");
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
+const factory = require("./handlerFactory");
 
-const User = require('../models/userModel');
+const User = require("../models/userModel");
 
 //Create User
 exports.createUser = factory.createOne(User);
 
-
 exports.getMe = (req, res, next) => {
-    req.params.id = req.user.id;
-    next();
-  };
+	req.params.id = req.user.id;
+	next();
+};
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+	const decoded = await promisify(jwt.verify)(
+		req.cookies.jwt,
+		process.env.JWT_SECRET
+	);
+	console.log(decoded);
+
+	if (req.body.password || req.body.passwordConfirm) {
+		return next(
+			new AppError(
+				"This route is not for password updates. Please updateMyPassword",
+				400
+			)
+		);
+	}
+	//2) Filterer out unwanted fields names that are not allowed to be updated
+	const filteredBody = filterObj(req.body, "username", "email");
+	console.log(req.files.photo);
+	const updateUser = await User.findByIdAndUpdate(
+		decoded.id,
+		filteredBody,
+		{
+			new: true,
+			runValidators: true,
+		}
+	);
+	res.status(200).json({
+		status: "success",
+		data: {
+			user: updateUser,
+		},
+	});
+});
