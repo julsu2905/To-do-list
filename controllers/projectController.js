68fd1945fc8149ed88dee72dc9cad6273e3adb19
sujul2905 <51090620+kurokami2905@users.noreply.github.com>
2020-11-02 21:11:31 +0700

    update

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,5 +1,34 @@
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
 const factory = require("./handlerFactory");
+const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
 
-const Project = require('../models/projectModel');
+const User = require("../models/userModel");
+const Project = require("../models/projectModel");
+
+exports.postProject = catchAsync(async (req, res, next) => {
+	const decoded = await promisify(jwt.verify)(
+		req.cookies.jwt,
+		process.env.JWT_SECRET
+	);
+
+	const newProject = {
+		projectName: req.body.projectName,
+        memberQuantity: req.body.memberQuantity,
+        description : req.body.description,
+		admin: decoded.id,
+	};
+    await Project.create(newProject);
+
+	const currentUser = await User.findByIdAndUpdate(
+		decoded.id,
+		{
+			$push: { project: newProject },
+		},
+		{
+			new: true,
+		}
+    );
+    res.status(201).redirect('/home');
+});
