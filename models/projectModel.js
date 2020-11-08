ffb9f53788e98e1bf3d0d5178cc64efe4de5cd77
sujul2905 <51090620+kurokami2905@users.noreply.github.com>
2020-11-08 22:09:31 +0700

    update model

--- models/projectModel.js
+++ models/projectModel.js
@@ -1,32 +1,36 @@
-const mongoose = require('mongoose');
-const projectController = require('../controllers/projectController');
+const mongoose = require("mongoose");
+const projectController = require("../controllers/projectController");
 
 const projectSchema = mongoose.Schema({
-    projectName : {
-        type : String,
-        required : true
-    },
-    memberQuantity : {
-        type : Number,
-        required : true
-    },
-    description : {
-        type : String,
-        required : false
-    },
-    admin : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'User'
-    },
-    tasks : [{
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'Task'
-    }],
-    members : [{
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'User'
-    }]
+	projectName: {
+		type: String,
+		required: true,
+	},
+	memberQuantity: {
+		type: Number,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: false,
+	},
+	admin: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+	},
+	tasks: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Task",
+		},
+	],
+	members: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+		},
+	],
 });
 
-const Project = mongoose.model('Project',projectSchema);
+const Project = mongoose.model("Project", projectSchema);
 module.exports = Project;
--- models/taskModel.js
+++ models/taskModel.js
@@ -1,28 +1,30 @@
-const mongoose = require('mongoose');
+const { ObjectId } = require("mongodb");
+const mongoose = require("mongoose");
 
 const taskSchema = mongoose.Schema({
-    taskName : {
-        type : String,
-        required : true
-    },
-    progress :{
-        enum : ['asigned' ,'working' ,'pending' ,'done'],
-        type : String,
-        default : 'asigned'
-    },
-    priority :{
-        type : String,
-        required : true
-    },
-    asignedMember :{
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'User'
-    },
-    dueDate :{
-        type : Date,
-        required : true
-    }
+	taskName: {
+		type: String,
+		required: true,
+	},
+	progress: {
+		enum: ["asigned", "working", "pending", "done"],
+		type: String,
+		default: "asigned",
+	},
+	priority: {
+		type: String,
+		enum: ["Critical", "Normal", "High", "Low"],
+		default: "Normal",
+	},
+	asignedMember: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+	},
+	dueDate: {
+		type: Date,
+		required: true,
+	},
 });
 
-const Task = mongoose.model('Task',taskSchema);
+const Task = mongoose.model("Task", taskSchema);
 module.exports = Task;
