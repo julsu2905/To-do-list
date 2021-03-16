d5af153e8cdac6cec0ebce7655eb734aee8ec342
Jul <69659361+julsu2905@users.noreply.github.com>
2021-03-16 10:42:50 +0700

    modify for local

--- public/js/login.js
+++ public/js/login.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { showAlert } from './alert';
 
 export const login =  (email, password) => {
-    const url ='http://pmware.herokuapp.com/api/login';  
+    const url ='http://127.0.0.1:9696/api/login';  
     axios({ 
             method: 'POST',
             url,
@@ -29,7 +29,7 @@ export const logout = async() => {
     try {
         const res = await axios({
             method: 'GET',
-            url: 'http://pmware.herokuapp.com/api/logout',
+            url: 'http://127.0.0.1:9696/api/logout',
         });
         if(res.data.status === 'success') {
             showAlert('success', 'Log out successfully');
